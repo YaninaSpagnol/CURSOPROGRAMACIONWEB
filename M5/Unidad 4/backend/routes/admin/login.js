ce8002da6f2b96792754b523bb44fe4e60609f7d
Yanina Spagnol <yaninaspagnol@gmail.com>
2021-11-02 01:36:24 -0300

    Modulo 5 Unidad 4

---
+++ M5/Unidad 4/backend/routes/admin/login.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModel');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin/login', {
+      layout: 'admin/layout'
+  });
+});
+
+/*logout*/
+router.get('/logout', function (req, res, next) {
+  req.session.destroy(); // destruir las variables de sesión (id y usuario)
+  res.render('admin/login', {
+    layout: 'admin/layout'
+  });
+});
+/*fin logout */
+
+
+router.post('/', async (req, res, next) => {
+  try {
+    var usuario = req.body.usuario; //Yanina
+    var password = req.body.password; //31071993
+
+    //console.log(req.body); //consola o terminal
+
+    var data = await 
+    usuariosModel.getUserByUsernameAndPassword(usuario, password);
+
+    if (data != undefined) {
+      req.session.id_usuario = data.id;
+      req.session.nombre = data.usuario;
+      res.redirect('/admin/novedades');
+    } else {
+      res.render('admin/login', {
+        layout: 'admin/layout',
+        error: true
+      });
+    }
+
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+module.exports = router;
